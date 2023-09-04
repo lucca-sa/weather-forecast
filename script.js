9b28232de7b34fff95a57eb9fd79145aacb68425
lucca-sa <scherer.lucca@gmail.com>
2023-09-04 18:16:44 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+const key =  "fd5279d236492a9014721f9483ac61e1"
+
+
+function searchClick() {
+    const city = document.querySelector(".city-input").value
+
+    searchCity(city)
+}
+
+
+async function searchCity(city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json() )
+     
+    showData(data)
+    console.log(data)
+ }
+
+
+ function showData(data){
+    document.querySelector(".tempo-em").innerHTML = "Tempo em " + data.name
+    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C"
+    document.querySelector(".tempo-previsao").innerHTML = data.weather[0].description
+    document.querySelector(".umidade").innerHTML = data.main.humidity + "% de umidade"
+    document.querySelector(".icone-tempo").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+}
